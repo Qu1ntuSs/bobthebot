41a5e5c4f9b1c64538a0a57a76b122319afb964b
Jakob Maier <Jakob Maier>
2017-03-19 17:47:40 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bobthebot',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Jakob Maier',
+      author_email='d33pshitter@gmail.com',
+      license='MIT',
+      packages=['bobthebot'],
+      zip_safe=False)
