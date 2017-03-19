ccb7fd302391d5d04b807b8da84f31a614df302d
Jakob Maier <Jakob Maier>
2017-03-19 18:44:45 +0100

    Change

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='bobthebot',
-      version='0.3',
+      version='0.5',
       description='The funniest joke in the world',
       author='Jakob Maier',
       author_email='d33pshitter@gmail.com',
       license='MIT',
       packages=['bobthebot'],
+	  scripts=['bin/funniest-joke', 'bin/unfunniest-joke.py'],
 	  install_requires=['markdown'],
       zip_safe=False)
